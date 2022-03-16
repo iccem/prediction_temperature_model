a3d2b043eb307488c78bb4e825783bc132cd1b28
lilF <delo5070@gmail.com>
2022-03-16 20:07:31 +0400

    loading date

--- load_data.py
+++ load_data.py
@@ -1,11 +1,6 @@
-import requests
+import wget
 
 class LoadData:
-    def get_data_url(url):
-        if url == '':
-            file_name = url.split('/')[-1]
-            
-            with open(file_name, 'wb') as file:
-                r = requests.get(url)
-                file.write(r.content)
-        return file_name
+    def get_data_url(self, url, file_content):
+        wget.download(url, file_content)
+
--- unzip_data.py
+++ unzip_data.py
@@ -1,7 +1,11 @@
 import gzip
+import shutil
 
 class UnGZIP:
-    def read_compressed_file(filename, file_content):
-        with gzip.open(filename, 'rb') as f:
-            file_content = f.read()
-            return file_content
+    def read_compressed_file(self, file_content):
+        name = file_content.split('.')
+        new_file_name = name[0]+'.'+name[1]
+        
+        with gzip.open(file_content, 'rb') as f:
+            with open(new_file_name, 'wb') as ff:
+                shutil.copyfileobj(f, ff)
