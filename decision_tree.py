1a6f43f50992c06a40b9d5fffe06b00511b7c8a1
lilF <delo5070@gmail.com>
2022-03-18 07:35:20 +0400

    decision_tree

--- decision_tree.py
+++ decision_tree.py
@@ -1,5 +1,13 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error
+
+file_name = 'weather.xls'
+data = pd.read_excel(file_name, skiprows=6)
+
+data = data[data['T'].notna()]
+data['date'] = pd.to_datetime(data['Местное время в Москве (ВДНХ)'], dayfirst=True)
+data_short = data[data['date'].between('2016-10-01', '2017-03-01')]
 
 data['dayofyear'] = data['date'].dt.dayofyear
 
